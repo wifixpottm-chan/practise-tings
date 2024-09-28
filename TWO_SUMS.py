fbb6d15b204f70f2dae22347f28febe6b9d07dec
Aarav Menon <116457209+wifixpottm-chan@users.noreply.github.com>
2024-09-28 11:23:20 +0530

    Create TWO_SUMS.py
    '''
    https://leetcode.com/problems/two-sum/description/
    
    Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
    You may assume that each input would have exactly one solution, and you may not use the same element twice.
    You can return the answer in any order.
    
    Example 1:
    
    Input: nums = [2,7,11,15], target = 9
    Output: [0,1]
    Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
    
    Example 2:
    Input: nums = [3,2,4], target = 6
    Output: [1,2]
    '''

---
+++ TWO_SUMS.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dic={}
+        for i in range(len(nums)):
+            if target-nums[i] in dic.keys():
+                return [i, dic[target-nums[i]]]
+            dic[nums[i]]=i
+        return []
